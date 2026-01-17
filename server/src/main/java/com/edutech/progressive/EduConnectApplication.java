884ac517aaa53ab636fca3cde3459ece51dbfb02
Venkat-2104 <dummy@gmail.com>
2026-01-17 06:10:33 +0000

    solve initial commit

---
+++ server/src/main/java/com/edutech/progressive/EduConnectApplication.java
@@ -0,0 +1,18 @@
+package com.edutech.progressive;
+
+// import com.edutech.progressive.entity.Teacher;
+// import com.edutech.progressive.service.impl.TeacherServiceImplArraylist;
+
+public class EduConnectApplication {
+    public static void main(String[] args) {
+        System.out.println("Welcome to EduConnect Progressive Project!");
+        // TeacherServiceImplArraylist teacherServiceImplArraylist = new TeacherServiceImplArraylist();
+        // Teacher teacher1 = new Teacher(1, "null", "null", "null", "null", 4);
+        // Teacher teacher2 = new Teacher(2, "null", "null", "null", "null", 3);
+        // teacherServiceImplArraylist.addTeacher(teacher1);
+        // teacherServiceImplArraylist.addTeacher(teacher2);
+        // // teacherServiceImplArraylist.emptyArrayList();
+        // System.out.println(teacherServiceImplArraylist.getAllTeachers());
+        // System.out.println(teacherServiceImplArraylist.getTeacherSortedByExperience());
+    }
+}
