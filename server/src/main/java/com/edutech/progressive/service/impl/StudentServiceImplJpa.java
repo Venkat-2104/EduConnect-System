2744a94372e71e6db9363fa1bde03ef4bda14d72
Venkat-2104 <dummy@gmail.com>
2026-01-28 17:52:18 +0000

    solve initial commit

--- server/src/main/java/com/edutech/progressive/service/impl/StudentServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/StudentServiceImplJpa.java
@@ -4,16 +4,12 @@ import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.edutech.progressive.dto.StudentDTO;
 import com.edutech.progressive.entity.Student;
 import com.edutech.progressive.exception.StudentAlreadyExistsException;
-import com.edutech.progressive.repository.AttendanceRepository;
-import com.edutech.progressive.repository.EnrollmentRepository;
 import com.edutech.progressive.repository.StudentRepository;
-import com.edutech.progressive.repository.UserRepository;
 import com.edutech.progressive.service.StudentService;
 
 @Service
